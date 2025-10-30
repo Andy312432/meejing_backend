d659d06f0973b1ac40c870589f8316cda01feae8
312432 <andy.wu.cyut.edu@gmail.com>
2025-10-30 21:57:50 +0800

    fix build 2

--- meejing_api/meejing_api/wsgi.py
+++ meejing_api/meejing_api/wsgi.py
@@ -6,8 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+if str(BASE_DIR) not in sys.path:
+    sys.path.append(str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
