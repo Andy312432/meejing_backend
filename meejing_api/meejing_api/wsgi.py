c316a171855646349a3c0a22bf7afac902384cdb
312432 <andy.wu.cyut.edu@gmail.com>
2025-10-30 22:18:22 +0800

    fix 7

--- meejing_api/meejing_api/wsgi.py
+++ meejing_api/meejing_api/wsgi.py
@@ -18,4 +18,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meejing_api.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
+application = app
