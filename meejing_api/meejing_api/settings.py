0db8dd965057817fb13431081791bdc35accd68d
312432 <andy.wu.cyut.edu@gmail.com>
2025-12-26 13:21:25 +0800

    fix: temporially remove page

--- meejing_api/meejing_api/settings.py
+++ meejing_api/meejing_api/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 999999,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
